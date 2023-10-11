34febdc74682f7be6bdbd45ea2a4d71cc0039d27
jyoti ranjan sharma <jyotiranjansharma11@gmail.com>
2023-10-11 19:40:27 +0530

    Add vanilla JavaScript & Redux

---
+++ index.js
@@ -0,0 +1,62 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+/**
+ * Action: Describe what happened, but don't describe the application's state changes.
+ */
+
+// String constant to describe the type of the action
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+// An action creator creates an action, its a function that returns an action.
+function orderCake() {
+  // define an action
+  return {
+    type: CAKE_ORDERED,
+    quantity: 2, // can be added optionally
+  };
+  // That is it action is created
+}
+
+/**
+ * Reducers: Specify how the application's state changes in response to actions sent to the store.
+ * Its a pure function that accepts state and action as arguments and returns the next state of the application.
+ * example: (previousState, action) => newState
+ */
+
+// Example State: Whenever the ownser opens the shop, there are 10 cakes on the shelf
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+    const quantity = action.quantity;
+  switch (action.type) {
+    case CAKE_ORDERED:
+      // make a copy of the state object and only update the numOfCakes
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - quantity,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+// All access to the state via getState()
+console.log("initial state", store.getState());
+// Registers listeners via subscribe(listener)
+const unsubscribe = store.subscribe(() => console.log("Updated state:", store.getState()));
+
+// Allows state to be updated via dispatch(action)
+// Invoke the orderCake action creator, which will inturn return the action
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+// Unsubscribe from the store, by calling the function returned by the subscribe method
+unsubscribe()
+
+// won't update the state
+store.dispatch(orderCake())
