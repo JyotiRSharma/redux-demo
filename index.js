5ca08dcb20abbd84605ef1f0f05bc588f09c0b99
jyoti ranjan sharma <jyotiranjansharma11@gmail.com>
2023-10-11 19:52:45 +0530

    Restore cakes

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ const createStore = redux.createStore;
 
 // String constant to describe the type of the action
 const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 
 // An action creator creates an action, its a function that returns an action.
 function orderCake() {
   // define an action
   return {
     type: CAKE_ORDERED,
-    quantity: 2, // can be added optionally
+    payload: 2, // can be added optionally
   };
   // That is it action is created
+}
+
+function restockCake(restockQuantity = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: restockQuantity,
+  };
 }
 
 /**
@@ -30,13 +38,18 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    const quantity = action.quantity;
+  const payload = action.payload;
   switch (action.type) {
     case CAKE_ORDERED:
       // make a copy of the state object and only update the numOfCakes
       return {
         ...state,
-        numOfCakes: state.numOfCakes - quantity,
+        numOfCakes: state.numOfCakes - payload,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + payload,
       };
     default:
       return state;
@@ -47,16 +60,21 @@ const store = createStore(reducer);
 // All access to the state via getState()
 console.log("initial state", store.getState());
 // Registers listeners via subscribe(listener)
-const unsubscribe = store.subscribe(() => console.log("Updated state:", store.getState()));
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state:", store.getState())
+);
 
 // Allows state to be updated via dispatch(action)
 // Invoke the orderCake action creator, which will inturn return the action
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+// Restock the cakes
+store.dispatch(restockCake());
 
 // Unsubscribe from the store, by calling the function returned by the subscribe method
-unsubscribe()
+unsubscribe();
 
 // won't update the state
-store.dispatch(orderCake())
+store.dispatch(orderCake());
